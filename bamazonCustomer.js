2a009c8427af91bc70b6d9eea2a21a7c4106f383
Naj Dhillon <najdhil86@gmail.com>
2019-09-26 15:18:12 -0700

    Reworked the entire app. I basically rewrote the whole thing from scratch. My attempt was successful in fixing bugs from my previous example ;

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,67 +1,85 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'tJvnjAtbMWtqW9HmmZBDGiZk',
-  database : 'bamazon',
-  port : 3306
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "bamazon",
+  port: 3306
 });
 
-connection.connect(function(err){
-  if (err) throw err;
+connection.connect();
 
-  showAllItems();
-  whatDoWantToBuyByID()
+connection.query("SELECT * FROM product", function(error, results, fields) {
+  if (error) throw error;
+
+  results[0].item_id;
+  results[0].price;
+
+  for (let i = 0; i < results.length; i++) {
+    console.log("---------");
+    console.log(
+      "~Product ID~: " +
+        results[i].item_id +
+        " ~Product Name~: " +
+        results[i].product_name +
+        " ~Product Price~: " +
+        results[i].price
+    );
+    console.log("---------");
+  }
 });
 
-// select everything from tables
+function buyProduct(id, user_quantity) {
+  connection.query("SELECT * FROM product WHERE item_id = ?", [id], function(
+    error,
+    buy_results,
+    fields
+  ) {
+    var item_id = buy_results[0].item_id;
+    var stock_quantity = buy_results[0].stock_quantity;
+    var updated_quantity = stock_quantity - user_quantity;
+    var price = buy_results[0].price;
+    var user_pricetotal = price * user_quantity;
 
-function showAllItems() { 
-  connection.query('SELECT item_id, product_name, price FROM product', function (error,results,fields) {
-    if (error) throw error;
+    if (user_quantity > stock_quantity) {
+      console.log("Insufficient quantity");
+      connection.end();
+    } else {
+      connection.query(
+        "UPDATE product SET stock_quantity = ? WHERE item_id = ?",
+        [updated_quantity, item_id],
+        function(error, update_results, fields) {
+          console.log("Product has been bought!!!");
+          console.log("Your total is " + user_pricetotal);
+        }
+      );
 
-    for(let i = 0; i<results.length; i++){
-      console.log("Product Name: " + results[i].product_name);
-      console.log("Product ID Number: " + results[i].item_id);
+      connection.end();
     }
   });
 }
 
-function whatDoWantToBuyByID() {
-  var questions = [
-    {type: "input",message: "What is the product id you would like to buy? ",name: "user_item_id"},
-    {type: "input",message: "How many would you like  to buy ? ",name: "user_item_quantity"},
+function AskUsers() {
+  var question = [
+    {
+      type: "input",
+      message: "What is the product id you would like to buy? ",
+      name: "user_item_id"
+    },
+    {
+      type: "input",
+      message: "How many would you like  to buy ? ",
+      name: "user_item_quantity"
+    }
   ];
-  
-  inquirer
-  .prompt(questions)
-  .then(function(response) {
-    var user_item_id = response.user_item_id;
-    var user_item_quantity = response.user_item_quantity;
-    
+  inquirer.prompt(question).then(function(uresp) {
+    var item_id = parseInt(uresp.user_item_id);
+    var item_quantity = parseInt(uresp.user_item_quantity);
 
-    connection.query('SELECT item_id, product_name, price,stock_quantity FROM product WHERE item_id = ?',[user_item_id], function(error,results,fields) {
-
-      // console.log(results);
-      var product_quantity = results[0].stock_quantity;
-      var product_price = results[0].price;
-      var total_price = product_price*user_item_quantity;
-      var new_quantity = product_quantity - user_item_quantity;
-      
-      if (product_quantity > user_item_quantity) {
-        connection.query("UPDATE product SET stock_quantity = ? WHERE item_id = ?",[new_quantity,response.user_item_id],function (err,results){
-          console.log("The total price of purchase is "+ total_price);
-        })
-      } else {
-        console.log("Insufficient quantity!")
-      }
-
-    })
-    connection.end();
+    buyProduct(item_id, item_quantity);
   });
 }
-// connection.end();
 
+AskUsers();
